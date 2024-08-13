4bba5dbb5d7145043484df6cd9cace40a99d7620
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-08-13 20:40:25 -0300

    🔧 feat: Set types and relative paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,20 @@
 import React, { useId } from "react";
 import ReactDOMServer from "react-dom/server";
+
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import { scaleLinear } from "d3-scale";
-import brazilTopoJson from "./brazil-topojson.json";
 import { Tooltip } from "react-tooltip";
+import { scaleLinear } from "d3-scale";
+
+import type { Geography as GeographyType, Metadata, MetaItem } from "./types";
+import { Legend } from "./components";
+
+import brazilTopoJson from "./brazil-topojson.json";
 
 import "./index.css";
 import "react-tooltip/dist/react-tooltip.css";
 
-export type Geography = {
-  type: string;
-  properties: {
-    name: string;
-    uf: string;
-    codigo: number;
-    regiao: string;
-  };
-  id: string;
-  rsmKey: string;
-};
-
-export type MetaItem = {
-  [key: string]: string | number;
-};
-
-export type Metadata = {
-  [key: string]: MetaItem;
-};
-
 interface HeatmapBrazilProps {
+  children?: React.ReactNode[] | React.ReactNode;
   data: Record<string, number>;
   metadata?: Metadata;
   colorRange?: [string, string];
@@ -40,11 +26,12 @@ interface HeatmapBrazilProps {
     hideArrow?: boolean;
     position?: "top" | "right" | "bottom" | "left";
   };
-  onClick?: (geo: Geography) => void;
+  onClick?: (geo: GeographyType) => void;
   tooltipContent?: (meta: MetaItem) => React.ReactNode;
 }
 
 function HeatmapBrazil({
+  children = [],
   data,
   metadata,
   domain,
@@ -58,11 +45,24 @@ function HeatmapBrazil({
   },
   tooltipContent,
 }: HeatmapBrazilProps): JSX.Element {
-  const id = useId().replace(/:/g, "");
+  const componentId = useId().replace(/:/g, "");
   const maxValue = Math.max(...Object.values(data));
   const colorScale = scaleLinear<string>()
     .domain(domain || [0, maxValue])
     .range(colorRange);
+
+  const renderLegend = (): React.ReactNode | null => {
+    if (Array.isArray(children)) {
+      return children.find(
+        (child) => React.isValidElement(child) && child.type === Legend
+      ) as React.ReactNode | null;
+    } else {
+      if (React.isValidElement(children) && children.type === Legend) {
+        return children;
+      }
+    }
+    return null;
+  };
 
   const getTooltipContent = (geoId: string): React.ReactNode => {
     if (tooltipContent && metadata) {
@@ -95,50 +95,53 @@ function HeatmapBrazil({
   };
 
   return (
-    <div className="react-brazil-heatmap">
-      <ComposableMap
-        className="w-100 h-100"
-        projection="geoMercator"
-        projectionConfig={{
-          scale: 1100,
-          center: [-54, -15],
-        }}
-      >
-        <Geographies geography={brazilTopoJson}>
-          {({ geographies }: { geographies: Geography[] }) =>
-            geographies.map((geo) => {
-              const stateValue = data[geo.id] || 0;
-              return (
-                <Geography
-                  className="react-brazil-heatmap__state"
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={colorScale(stateValue)}
-                  id={`geo-${id}-${geo.id}`}
-                  data-tooltip-id={`tooltip-${id}`}
-                  data-tooltip-html={ReactDOMServer.renderToStaticMarkup(
-                    getTooltipContent(geo.id)
-                  )}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ComposableMap>
-      <Tooltip
-        id={`tooltip-${id}`}
-        hidden={!tooltip.show}
-        openOnClick={tooltip.trigger === "click"}
-        float={tooltip.float}
-        place={tooltip.position}
-        style={{
-          padding: 0,
-          backgroundColor: "transparent",
-          border: "none",
-        }}
-      />
-    </div>
+      <div className="react-brazil-heatmap">
+        <ComposableMap
+          className="w-100 h-100"
+          projection="geoMercator"
+          projectionConfig={{
+            scale: 1100,
+            center: [-54, -15],
+          }}
+        >
+          <Geographies geography={brazilTopoJson}>
+            {({ geographies }: { geographies: GeographyType[] }) =>
+              geographies.map((geo) => {
+                const stateValue = data[geo.id] || 0;
+                return (
+                  <Geography
+                    className="react-brazil-heatmap__state"
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={colorScale(stateValue)}
+                    id={`geo-${componentId}-${geo.id}`}
+                    data-tooltip-id={`tooltip-${componentId}`}
+                    data-tooltip-html={ReactDOMServer.renderToStaticMarkup(
+                      getTooltipContent(geo.id)
+                    )}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        </ComposableMap>
+        {renderLegend()}
+        <Tooltip
+          id={`tooltip-${componentId}`}
+          hidden={!tooltip.show}
+          openOnClick={tooltip.trigger === "click"}
+          float={tooltip.float}
+          place={tooltip.position}
+          border="none"
+          style={{
+            padding: 0,
+            backgroundColor: "transparent",
+          }}
+        />
+      </div>
   );
 }
 
 export default HeatmapBrazil;
+export type { GeographyType, Metadata, MetaItem };
+export { Legend } from "./components";
